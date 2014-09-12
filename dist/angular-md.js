441f698affd624845828a308982d890e2d53502e
Steven Lambion <steven.lambion@atsid.com>
2014-09-12 13:11:49 -0700

    build to add encodeURI

--- dist/angular-md.js
+++ dist/angular-md.js
@@ -296,7 +296,7 @@ angular.module("atsid.data.store").provider("httpStore", [function () {
 
                 url = baseUrl + separator + url;
                 angular.forEach(query, function (value, name) {
-                    queryList.push(name + "=" + value);
+                    queryList.push(name + "=" + encodeURIComponent(value));
                 });
                 if (queryList.length) {
                     url += "?" + queryList.join("&");
